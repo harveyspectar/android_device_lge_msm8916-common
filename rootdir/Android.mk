d0fbab5d473738c9ad23f93601cd08b9a5eddee3
Erfan Abdi <erfangplus@gmail.com>
2017-08-05 13:36:30 +0430

    msm8916: sensord improvment
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -36,14 +36,6 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.board.fm.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.board.fm.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE            := init.board.sensors.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
